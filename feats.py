c59fe735c9727baf4fc5bd54c8bcd8cbd0d0e589
alyssahwang <ahh2143@columbia.edu>
2018-09-17 14:24:39 -0400

    Start feats with dummy examples

--- feats.py
+++ feats.py
@@ -6,4 +6,28 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelBinarizer
 import sklearn
 import crfsuite
-import spacy
+# import spacy
+
+# Functions to extract features
+# For the ith word in a sentence, return list of features
+def word2feats(sent, i) :
+	word = sent.split()[i]
+	feats = [
+		"word.upper=" + word.upper(),
+		"word.islower=%s" % word.islower()
+	]
+
+	return feats
+
+# Return list of lists of features for each word in a sentence
+def sent2feats(sent) :
+	return [word2feats(sent, i) for i in range(len(sent.split()))]
+
+# Import dataset
+sampledata = ["i am hungry", "computer science is cool"]
+
+for l in sampledata :
+	print(sent2feats(l))
+
+# CRFsuite tutorial from github
+# https://github.com/scrapinghub/python-crfsuite/blob/master/examples/CoNLL%202002.ipynb
