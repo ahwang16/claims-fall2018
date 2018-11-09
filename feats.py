0d0c3fedbd24b0eba1dbd514f67b22f2c8e3419a
alyssahwang <ahh2143@columbia.edu>
2018-11-09 15:46:31 -0500

    fix indent issue

--- feats.py
+++ feats.py
@@ -54,13 +54,13 @@ def sent2feats(sent) :
 def classify(y_true, y_pred):
 	lb = LabelBinarizer()
 	y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
-    y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
-        
-    tagset = set(lb.classes_) - {'O'}
-    tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
-    class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
-    
-    return classification_report(
+	y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+
+	tagset = set(lb.classes_) - {'O'}
+	tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+	class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+
+	return classification_report(
         y_true_combined,
         y_pred_combined,
         labels = [class_indices[cls] for cls in tagset],
