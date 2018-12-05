d0731ae389dc6781c1e15d94977a137c1bf7cd43
alyssahwang <ahh2143@columbia.edu>
2018-12-05 09:01:52 -0500

    sent parsing sentiment

--- parsesentiment.py
+++ parsesentiment.py
@@ -25,13 +25,15 @@ type ann_name: (file name of) annotation file, annotations separated by tabs
 rtype sents: list of subjective phrases (string)
 rtype labels: list of positive, neutral, negative, none polarity labels for each phrase
 '''
-def parse(doc_name, ann_name):
+def parse(doc_name, ann_name, sent_name):
 	sents = []
+	phrases = []
 	labels = []
 
 	# open files
 	doc = open(doc_name, "rb")
 	ann = open(ann_name, "r")
+	sen = open(sent_name, "f")
 	out = open("sentimentphrases.txt", "w")
 
 	# skip initial comment lines
@@ -69,17 +71,42 @@ def parse(doc_name, ann_name):
 					phrase += doc.read(1).decode('utf-8')
 					start += 1
 
-				sents.append(phrase)
+				phrases.append(phrase)
 
 				out.write("{} {}\n".format(pol.upper(), phrase))
 		
 		line = ann.readline()
 
-	return sents, labels
+
+	while True:
+		line = sen.readline()
+		if not line.startswith("#"):
+			break
+
+	while line:
+		l = line.split('\t')
+
+		span = l[1].split(",")
+		start, stop = int(span[0]), int(span[1])
+		doc.seek(start)
+		sent = ""
+
+		while start < stop:
+			sent += doc.read(1).decode('utf-8')
+			start += 1
+
+		sents.append(sent)
+
+		out.write("{}\n").format(sent)
+
+		line = sen.readline()
+
+	return sents, phrases, labels
 
 
 if __name__ == "__main__":
 	ann = "/proj/nlp/users/alyssa/claims-fall2018/database.mpqa.1.2/man_anns/20010620/13.40.05-15087/gateman.mpqa.lre.1.2"
 	doc = "/proj/nlp/users/alyssa/claims-fall2018/database.mpqa.1.2/docs/20010620/13.40.05-15087"
+	sen = "/proj/nlp/users/alyssa/claims-fall2018/database.mpqa.1.2/man_anns/20010620/13.40.05-15087/gatesentences.mpqa.1.2"
 
 	parse(doc, ann)
